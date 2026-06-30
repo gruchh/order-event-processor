ebe8bff25700fa4b1c5d60e89eec7fb8a383e24b
gruchh <kubol2@gmail.com>
2026-06-30 20:14:33 +0200

    first commit

---
+++ backend/src/test/java/com/kafka/dlt/DltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.dlt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
